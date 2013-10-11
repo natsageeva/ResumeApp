3c14b5d32692a920d446fd244c1079364e28020c
Nats <nats.ageeva@gmail.com>
2013-10-12 00:26:04 +0400

    from IDEA

---
+++ ResumeApp/src/main/java/com/natsageeva/resumeapp/ClassFromIdea.java
@@ -0,0 +1,13 @@
+package com.natsageeva.resumeapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nats
+ * Date: 12.10.13
+ * Time: 0:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassFromIdea
+{
+
+}
